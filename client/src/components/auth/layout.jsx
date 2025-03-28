da128b6ebb15ab86f609f832ebcca10eeba485e1
wachira567 <wachiramboche@gmail.com>
2025-03-28 21:47:14 +0300

    shopkenya

--- client/src/components/auth/layout.jsx
+++ client/src/components/auth/layout.jsx
@@ -6,7 +6,7 @@ function AuthLayout() {
       <div className="hidden lg:flex items-center justify-center bg-black w-1/2 px-12">
         <div className="max-w-md space-y-6 text-center text-primary-foreground">
           <h1 className="text-4xl font-extrabold tracking-tight">
-            Welcome to ECommerce Shopping
+            Welcome to ShopKenya
           </h1>
         </div>
       </div>
